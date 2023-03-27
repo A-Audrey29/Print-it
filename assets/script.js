2cc8d869462f54a07c43c496be6fff870a537350
Audrey <appere.audrey29@gmail.com>
2023-03-27 14:29:38 +0200

    ajout des images slider et test sur le JS

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,18 @@
 const slider = document.querySelector(".banner");
 const previousButton = document.querySelector(".arrow_left");
 const nextButton = document.querySelector(".arrow_right");
+const bulletSelector = document.querySelector(".dot_selected")
+const bullet = document.querySelector(".dot")
 
 
+// flêche de gauche
 // flêche de gauche
 function previous (){
 	
 };
 
 previousButton.addEventListener("onclick",previous);
-alert("à gauche ok");	
+// alert("à gauche ok");	
 
 // flêche de droite
 function next (){
@@ -17,7 +20,30 @@ function next (){
 };
 
 nextButton.addEventListener("onclick",next);
-alert("à droite ok");	
+// alert("à droite ok");	
+
+
+// insert des images
+ slides.map(slides, index =>{
+	const imageElement = document.createElement('img'); //création de l'emplacement des images dans le dom
+ 	imageElement.src = slides; // on donne la source des images a afficher
+	imageElement.id = index; // donne un numéro dans le tableau
+ 	imageElement.classList.add("banner-img"); // ajout du css
+ 	slider.appendChild(imageElement); // insertion du slider
+})
+
+// boucle pour parcourir l'ensemble du tableau
+let scroll = slides;
+for (var i = 0;i<slides.length; i++){
+	console.log(scroll[i]);
+};
+
+// fonction pour passer à la suivante
+var slideIndex = 0;
+
+
+
+// insert des bullet points
 
 
 
@@ -38,4 +64,4 @@ const slides = [
 		"image":"slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
-]
+];
