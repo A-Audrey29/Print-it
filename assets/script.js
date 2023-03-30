4bc418c747bcdcf7bf5bec93c0daa4eefda1865e
Audrey <appere.audrey29@gmail.com>
2023-03-30 13:58:10 +0200

    ajout de la boucle for pour parcourir le tableau

--- assets/script.js
+++ assets/script.js
@@ -1,67 +1,54 @@
-const slider = document.querySelector(".banner");
-const previousButton = document.querySelector(".arrow_left");
-const nextButton = document.querySelector(".arrow_right");
-const bulletSelector = document.querySelector(".dot_selected")
-const bullet = document.querySelector(".dot")
+const  slideShow = document.getElementById("banner")
+const slide = document.querySelector(".banner-img")
+const previousSlide = document.getElementById("previous_btn")
+const nextSlide = document.getElementById("next_btn")
+const slider = document.querySelector(".slider")
 
-
-// flêche de gauche
-// flêche de gauche
-function previous (){
-	
-};
-
-previousButton.addEventListener("onclick",previous);
-// alert("à gauche ok");	
-
-// flêche de droite
-function next (){
-	
-};
-
-nextButton.addEventListener("onclick",next);
-// alert("à droite ok");	
-
-
-// insert des images
- slides.map(slides, index =>{
-	const imageElement = document.createElement('img'); //création de l'emplacement des images dans le dom
- 	imageElement.src = slides; // on donne la source des images a afficher
-	imageElement.id = index; // donne un numéro dans le tableau
- 	imageElement.classList.add("banner-img"); // ajout du css
- 	slider.appendChild(imageElement); // insertion du slider
+nextSlide.addEventListener("click", () => {
+	const slideWidth = slide.clientWidth
+	slideShow.scrollLeft += slideWidth
+	console.log(slideWidth)
 })
 
-// boucle pour parcourir l'ensemble du tableau
-let scroll = slides;
-for (var i = 0;i<slides.length; i++){
-	console.log(scroll[i]);
-};
-
-// fonction pour passer à la suivante
-var slideIndex = 0;
-
-
-
-// insert des bullet points
-
-
+previousSlide.addEventListener("click", () => {
+	const slideWidth = slide.clientWidth
+	slideShow.scrollLeft -= slideWidth
+	console.log(slideWidth)
+	previousSlide.classList.add(".banner-img")
+})
 
 const slides = [
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		src:"./assets/images/slideshow/slide1.jpg",
+		tagLine:"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		src:"./assets/images/slideshow/slide2.jpg",
+		tagLine:"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		src:"./assets/images/slideshow/slide3.jpg",
+		tagLine:"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		src:"./assets/images/slideshow/slide4.png",
+		tagLine:"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ];
+
+
+for (let index = 0; index < slides.length; index++) {
+	const data = slides[index]
+	const image = document.createElement("img") 
+	image.setAttribute("src",data.src)
+	console.log(image)
+	banner.appendChild(image)
+	banner.classList.add(".banner")
+}
+
+
+
+// const cle = "image"
+// console.log(slides[2][cle]);
+// console.log(slides[2].tagLine);
+// console.log(image)
